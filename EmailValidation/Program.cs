831d360f9a4eba946bbd30bb326f59696726ddc5
S Soundarya <soundarya.s@tvsnext.com>
2023-08-02 21:49:42 +0530

    [Soundarya]Add:Validate email

---
+++ EmailValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateEmail validateEmail = new ValidateEmail();
+            Console.WriteLine("Enter the Email to be validated:");
+            string input = Console.ReadLine();
+            validateEmail.Validate(input);
+        }
+    }
+}
---
+++ EmailValidation/ValidateEmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidation
+{
+    public class ValidateEmail
+    {
+        string actual = "^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,3}([.]{1}[a-zA-Z0-9]{2}){0,1}$";
+        public void Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, actual);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+
+    }
+}
